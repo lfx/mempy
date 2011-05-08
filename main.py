60baa3e78a9fe1e8d214a334e5f5521164e09300
Liudas Sodonis <liudas@sodonis.lt>
2011-05-08 14:03:02 +0300

    Hmm, why this don't commit?

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 import psutil
 import sys
 
-def get_mem(_name):
-#		print _name
-		pids = psutil.get_pid_list()
-		mem = 0
-		mb = 1024*1024
-		for i in pids:
-			try:
-				p = psutil.Process(i)
-			#	print p.name.upper()
-				if p.name.upper().find(_name.upper()) > -1:
-				#	print p.name
-					m=p.get_memory_info()
-					mem+=m[0]
-			except:
-				pass
+mb = 1024*1024
 
-		print 'Memory usage for '+_name+' is: '+str(mem/mb)+' Mb'	
+def get_mem(_name):
+    pids = psutil.get_pid_list()
+    mem = 0
+    for i in pids:
+        try:
+            p = psutil.Process(i)
+            if p.name.upper().find(_name.upper()) > -1:
+                m=p.get_memory_info()
+                mem+=m[0]
+        except:
+            pass
+    
+    print 'Memory usage for '+_name+' is: '+str(mem/mb)+' Mb'	
 
 if __name__=='__main__':
-	#	get_mem('google')
-	if len(sys.argv)>1:
-		get_mem(sys.argv[1])
-	else:
-		print 'Non app selected. Use "mem2.py google" or etc. '
+    if len(sys.argv)>1:
+        get_mem(sys.argv[1])
+    else:
+        print 'Non app selected. Use "mem2.py google" or etc. '        
